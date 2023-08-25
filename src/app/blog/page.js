ae8f55c90fd2f551b8f2438738087a712e65c3b2
wproman <rmn.tda@gmail.com>
2023-08-25 15:46:06 +0600

    x

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -9,13 +9,15 @@ export default async function Blog () {
     <div className='mt-5 grid-cols-2 grid space-x-4 '>
     {lists.map(list =>{
         return(<div key= {list.id} >
+            <Link href={`/single-blog/${list.id}`}>
         <img src={list.img}/>
         <h1 className="font-bold">
-        <Link href={`/single-blog/${list.id}`}>
+       
         {list.title}
-        </Link>
+       
         </h1>
         {list.short}
+        </Link>
         </div>)
         
     })}
